c97b368f1ffbccd0c3573c403cecf73f92c98331
daregoodness1997 <daregoodness@gmail.com>
2023-07-29 17:50:40 +0100

    initial commit

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,133 @@
+import { Button, Divider, Input, Text } from "@nextui-org/react";
+import React from "react";
+import { CheckIcon } from "../icons/CheckIcon";
+import { Box } from "../styles/box";
+import { Flex } from "../styles/flex";
+
+export const Hero = () => {
+  return (
+    <div style={{ position: "absolute", top: "0" }}>
+      <video
+        width="750"
+        height="500"
+        autoPlay
+        loop
+        muted
+        className="background-video"
+      >
+        <source type="video/mp4" src="assets/video/hero-video.mp4" />
+      </video>
+      <Flex
+        css={{
+          gap: "$3",
+          px: "$6",
+          mt: "$20",
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          position: "relative",
+          zIndex: 10,
+          "@sm": {
+            flexDirection: "row",
+            mt: "$20",
+          },
+        }}
+        justify={"center"}
+      >
+        <Box
+          css={{
+            pt: "$13",
+
+            display: "flex",
+            flexDirection: "column",
+            gap: "$5",
+            textAlign: "center",
+          }}
+        >
+          <Box
+            css={{
+              maxWidth: "800px",
+            }}
+          >
+            <Text
+              h1
+              css={{
+                display: "inline",
+                fontSize: "48px",
+                lineHeight: "1.25",
+                "@sm": {
+                  fontSize: "96px",
+                  lineHeight: "1.25",
+                },
+              }}
+            >
+              Want to join{" "}
+            </Text>
+            <Text
+              h1
+              css={{
+                display: "inline",
+                fontSize: "48px",
+                lineHeight: "1.25",
+
+                "@sm": {
+                  fontSize: "96px",
+                  lineHeight: "1.25",
+                },
+              }}
+            >
+              for{" "}
+            </Text>
+            <Text
+              h1
+              css={{
+                display: "inline",
+                fontSize: "48px",
+                lineHeight: "1.25",
+
+                "@sm": {
+                  fontSize: "96px",
+                  lineHeight: "1.25",
+                },
+              }}
+              color="primary"
+            >
+              Nistre Academy
+            </Text>
+          </Box>
+
+          <Text
+            css={{
+              color: "$accents8",
+              textAlign: "center",
+              maxWidth: "800px",
+            }}
+            size={"$lg"}
+            span
+          >
+            Our passion is developing young stars to fufilling their potential
+            in footabll
+          </Text>
+
+          <Flex
+            css={{
+              gap: "$8",
+              pt: "$4",
+              maxWidth: "800px",
+              justifyContent: "center"
+            }}
+            wrap={"wrap"}
+          >
+            <Input placeholder="Enter your email address" size="lg" />
+            <Button>Contact Us</Button>
+          </Flex>
+        </Box>
+      </Flex>
+      <Divider
+        css={{ position: "absolute", inset: "0p", left: "0", mt: "$10" }}
+      />
+    </div>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const lightTheme = createTheme({
+    type: "light",
+    theme: {
+      colors: {},
+    },
+  });
+
+  const darkTheme = createTheme({
+    type: "dark",
+    theme: {
+      colors: {},
+    },
+  });
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
